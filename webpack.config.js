c7c58f60d46e671f03ea290a84fce650f46542e2
Skati <drozdovsv87@gmail.com>
2020-08-25 22:51:46 +0300

    feat: webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключили плагин
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    // настроили плагин
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+};
