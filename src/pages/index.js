ed21c52840902b9d065c0402c86df5e3712d5849
Skati <drozdovsv87@gmail.com>
2020-08-27 02:29:26 +0300

    feat: webpack

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
-﻿import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
-import './index.css';
+﻿import Card from "../components/Card.js";
+import FormValidator from "../components/FormValidator.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
+import "./index.css";
 import {
   initialCards,
   validateSettings,
@@ -17,38 +17,44 @@ import {
   cardListContainer,
   buttonAdd,
   imageLink,
-  imageName
-} from '../components/constants.js';
+  imageName,
+} from "../components/constants.js";
 
-
-const ProfileValidation = new FormValidator(validateSettings, 'form[name="profile"]');
-const AddCardValidation = new FormValidator(validateSettings, 'form[name="add_card"]');
-const ImagePopup = new PopupWithImage('.popup_type_photo');
+const ProfileValidation = new FormValidator(
+  validateSettings,
+  'form[name="profile"]'
+);
+const AddCardValidation = new FormValidator(
+  validateSettings,
+  'form[name="add_card"]'
+);
+const ImagePopup = new PopupWithImage(".popup_type_photo");
 const userInfo = new UserInfo({
-  userName : profileName,
-  userDescription : profileDescription
+  userName: profileName,
+  userDescription: profileDescription,
 });
 
-const UserPopup = new PopupWithForm('.popup_type_profile', ()=>{
+const UserPopup = new PopupWithForm(".popup_type_profile", () => {
   userInfo.setUserInfo();
 });
 
-const cardList = new Section({
-  items: initialCards,
-  renderer: (item) => {
-    const card = new Card(item.name, item.link,(item)=>{
-      ImagePopup.open(item);
-      ImagePopup.setEventListeners();
-    });
-    const cardElement = card.generateCard();
-    cardList.addItem(cardElement);
+const cardList = new Section(
+  {
+    items: initialCards,
+    renderer: (item) => {
+      const card = new Card(item.name, item.link, (item) => {
+        ImagePopup.open(item);
+        ImagePopup.setEventListeners();
+      });
+      const cardElement = card.generateCard();
+      cardList.addItem(cardElement);
     },
   },
   cardListContainer
 );
 
-const AddCardPopup = new PopupWithForm('.popup_type_add-card', ()=>{
-  const card = new Card(imageName.value,imageLink.value,(item)=>{
+const AddCardPopup = new PopupWithForm(".popup_type_add-card", () => {
+  const card = new Card(imageName.value, imageLink.value, (item) => {
     ImagePopup.open(item);
   });
   const cardElement = card.generateCard();
@@ -57,20 +63,20 @@ const AddCardPopup = new PopupWithForm('.popup_type_add-card', ()=>{
 
 AddCardPopup.setEventListeners();
 
-buttonEdit.addEventListener('click', () => {
+buttonEdit.addEventListener("click", () => {
   ProfileValidation.enableValidation();
-  nameInput.value   = userInfo.getUserInfo().name;
+  nameInput.value = userInfo.getUserInfo().name;
   jobInput.value = userInfo.getUserInfo().info;
   UserPopup.open();
   UserPopup.setEventListeners();
 });
 
-editFormPopup.addEventListener('submit',()=>{
+editFormPopup.addEventListener("submit", () => {
   userInfo.setUserInfo();
   UserPopup.close();
 });
 
-buttonAdd.addEventListener('click', () => {
+buttonAdd.addEventListener("click", () => {
   AddCardPopup.open();
   AddCardValidation.enableValidation();
 });
